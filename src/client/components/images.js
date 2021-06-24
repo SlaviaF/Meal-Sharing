58990952e4fc4fad5f0440cb817d94415c9add3d
SlaviaF <slaviafurtado@gmail.com>
2021-06-24 09:44:47 +0200

    images added through this component

---
+++ src/client/components/images.js
@@ -0,0 +1,82 @@
+const images = [
+    {
+        id: 1,
+        img:"https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-korma-1601325562.png"
+    },
+    {
+        id: 2,
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Espaguetis_carbonara.jpg/1200px-Espaguetis_carbonara.jpg"
+
+    },
+    {
+        id:3,
+        img:"https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/meatball_tagine_45314_16x9.jpg"
+    },
+    {
+        id: 4,
+        img:"//www.indianhealthyrecipes.com/wp-content/uploads/2014/11/paneer-butter-masala-recipe-2.jpg"
+    },
+    {
+        id: 5,
+        img:"https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcac.2F2020.2F01.2F15.2F3c1ac8b2-9585-4eee-b5c9-ed90ca17b33f.2Ejpeg/750x562/quality/80/crop-from/center/cr/wqkgTWFyaWVsbGUgLyBQaG90b2N1aXNpbmUgLyBDdWlzaW5lIEFjdHVlbGxl/cocotte-de-boeuf-bourguignon.jpeg"
+    },
+    {
+        id: 6,
+        img: "https://img.delicious.com.au/d7tXjXK0/del/2015/10/bun-cha-16771-2.jpg"
+    },
+    {
+        id: 7,
+        img: "https://www.wellplated.com/wp-content/uploads/2019/08/Eggplant-Lasagna-Recipe.jpg"
+    },
+    {
+        id: 8,
+        img: "https://preview.redd.it/bofe33q82xl61.jpg?auto=webp&s=c4ed21976fb6d8d58fb6bf99b1b2e7c038fb1d2f"
+    },
+    {
+        id: 9,
+        img:"https://bryonyskitchen.files.wordpress.com/2016/11/img_7957.jpg?w=1024&h=683"
+    },
+    {
+        id: 10,
+        img:"https://i1.wp.com/www.dinedelicious.in/wp-content/uploads/2020/05/Veg-Biryani-Recipe.jpg?fit=1000%2C766&ssl=1"
+    },
+    {
+        id: 11,
+        img:"//artofpalate.com/wp-content/uploads/2020/06/mutton-biryani-500x500.jpg"
+    },
+    {
+        id: 12,
+        img:"https://inquiringchef.com/wp-content/uploads/2020/02/Thai-Massaman-Curry_1.jpg"
+    },
+    {
+        id: 13,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIn3MTFcIdYsf5JEmj79PYCfGlD4sM1PuuBw&usqp=CAU"
+    },
+    {
+        id: 14,
+        img:"http://maunikagowardhan.co.uk/wp-content/uploads/2016/02/Hyderabadi-Murgh-Kaju-Korma-700x467.jpg"
+    },
+    {
+        id: 15,
+        img:"https://figandoliveplatter.com/wp-content/uploads/2020/01/MG_2575-1024x768.jpg"
+    },
+    {
+        id: 16,
+        img:"https://images.immediate.co.uk/production/volatile/sites/30/2020/08/thai-red-curry-34c1e6d.jpg?quality=90&resize=440,400"
+    },
+    {
+        id: 17,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgIgMBC8TwXrypXY49fUZopT9gzAN44gKHlQ&usqp=CAU"
+    },
+    {
+        id: 18,
+        img:"https://www.whiskaffair.com/wp-content/uploads/2020/04/Kadai-Chicken-3.jpg"
+    },
+    {
+        id: 19,
+        img:"https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+
+]
+
+export default images
